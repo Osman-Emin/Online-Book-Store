4b7cf38ba6cef3a56e305870c7da223fa258097c
Ahmad BD <ahmad2135@gmail.com>
2021-08-11 11:34:55 +0300

    styling delete confirmation dialog

--- AdminPanel/wwwroot/js/site.js
+++ AdminPanel/wwwroot/js/site.js
@@ -2,3 +2,41 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    function ConfirmDialog(message) {
+        var def = $.Deferred();
+        var result = false;
+        $('<div></div>').appendTo('body')
+            .html('<div><h6>' + message + '?</h6></div>')
+            .dialog({
+                modal: true,
+                title: 'Confirm',
+                zIndex: 10000,
+                autoOpen: true,
+                width: 'auto',
+                resizable: false,
+                buttons: {
+                    Yes: function() {
+                        result = true;
+
+                        $(this).dialog("close");
+                    },
+                    No: function() {
+                        result = false;
+
+                        $(this).dialog("close");
+                    }
+                },
+                close: function(event, ui) {
+                    $(this).remove();
+                    if(result) 
+                        def.resolve();
+                    else def.reject();
+                }
+            });
+        // create and/or show the dialog box here
+        // but in "OK" do 'def.resolve()'
+        // and in "cancel" do 'def.reject()'
+
+        return def.promise();
+    
+}
